a005bb7dea41e122606633a1e72190db5671d711
Andrea Ligorio <andrea85kro@gmail.com>
2021-03-30 16:33:34 +0200

    Bonus 1 completato

--- js/main.js
+++ js/main.js
@@ -2,16 +2,25 @@ Vue.config.devtools = true;
 var app = new Vue({
     el: "#app",
     data: {
-        cards: {}
+        cards: {},
+        genres: [],
+        selected: "All"
     },
     methods: {
-
+        genreArray() {
+            for(let i = 0; i < this.cards.length; i++) {
+                if(!this.genres.includes(this.cards[i].genre))
+                this.genres.push(this.cards[i].genre);
+            }
+            this.genres.sort();
+        }
     },
     mounted() {
         axios
             .get("https://flynn.boolean.careers/exercises/api/array/music")
             .then((response) => {
                 this.cards = response.data.response;
+                this.genreArray();
             });
     }
 });
